3b6f2be49de661ed6eee23166b89973dd8e76d5d
MariamElenna <33805466+MariamElenna@users.noreply.github.com>
2026-06-23 15:46:33 +0300

    update: release version

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Framework",
-            url: "https://github.com/MariamElenna/FrameworkBinary/releases/download/1.0.0/Framework.xcframework.zip",
+            url: "https://github.com/MariamElenna/FrameworkBinary/releases/download/1.0.3/Framework.xcframework.zip",
             checksum: "62c4cc1cfc23a8b54eb082119e3a9fb5c0dc7d81b5edcd519f44d21960029a2e"
         )
     ]
