460c1f3a85da99569bafac3e6f3549c284af024d
MariamElenna <33805466+MariamElenna@users.noreply.github.com>
2026-06-23 15:27:26 +0300

    feat: add Package.swift

---
+++ Package.swift
@@ -0,0 +1,26 @@
+//
+//  Package.swift
+//  
+//
+//  Created by Mariam Elenna on 23/06/2026.
+//
+
+import Foundation
+// Package.swift
+
+let package = Package(
+    name: "Framework",
+    products: [
+        .library(
+            name: "Framework",
+            targets: ["Framework"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Framework",
+            url: "https://github.com/company/FrameworkBinary/releases/download/1.0.0/Framework.xcframework.zip",
+            checksum: "الـ checksum"
+        )
+    ]
+)
