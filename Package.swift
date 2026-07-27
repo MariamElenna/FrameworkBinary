14ee8b1ba975716abb03835df98ce38f97715861
MariamElenna <33805466+MariamElenna@users.noreply.github.com>
2026-07-27 16:59:17 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     products: [
         .library(
             name: "Framework",
-            targets: ["Framework"]
+            targets: ["FrameworkWrapper"]
         )
     ],
     dependencies: [
@@ -21,9 +21,17 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "Framework",
+            name: "FrameworkBinary",
             url: "https://github.com/MariamElenna/FrameworkBinary/releases/download/1.0.5/Framework.xcframework.zip",
             checksum: "8f0ac0f29c2ab12bff437fb3e7322520b7b6754dedca586d34c20977225b7967"
+        ),
+        .target(
+            name: "FrameworkWrapper",
+            dependencies: [
+                "FrameworkBinary",
+                .product(name: "AmplitudeSwift", package: "Amplitude-Swift")
+            ],
+            path: "Sources/FrameworkWrapper"
         )
     ]
 )
