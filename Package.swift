71cc89893424c958f945e3d36578897c85a561c2
MariamElenna <33805466+MariamElenna@users.noreply.github.com>
2026-07-27 15:42:10 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,16 +4,25 @@ import PackageDescription
 
 let package = Package(
     name: "Framework",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
         .library(
             name: "Framework",
             targets: ["Framework"]
         )
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/amplitude/Amplitude-Swift.git",
+            from: "1.18.6"
+        )
+    ],
     targets: [
         .binaryTarget(
             name: "Framework",
-            url: "https://github.com/MariamElenna/FrameworkBinary/releases/download/1.0.3/Framework.xcframework.zip",
+            url: "https://github.com/MariamElenna/FrameworkBinary/releases/download/1.0.4/Framework.xcframework.zip",
             checksum: "62c4cc1cfc23a8b54eb082119e3a9fb5c0dc7d81b5edcd519f44d21960029a2e"
         )
     ]
